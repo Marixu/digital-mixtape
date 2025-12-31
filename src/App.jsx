11310666d06ffa4ee1246d9dd5470ea3e4539825
Marixu <marrex9@hotmail.com>
2025-12-31 11:02:53 +0100

    move left

--- src/App.jsx
+++ src/App.jsx
@@ -3052,7 +3052,7 @@ if (isMobile) {
 
 
 {currentPage === "home" && (
-  <div style={{ width: "100%", display: "flex", justifyContent: "flex-start", alignItems: "flex-start",paddingLeft: "20px" }}> 
+  <div style={{ width: "100%", display: "flex", justifyContent: "flex-start", alignItems: "flex-start" }}> 
   <div
   style={{
     ...styles.main,
