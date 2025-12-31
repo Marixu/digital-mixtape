8cb9541db53cc9c6f1a9aeb7bc6642daabf64720
Marixu <marrex9@hotmail.com>
2025-12-31 13:56:51 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3720,7 +3720,7 @@ if (isMobile) {
     width: "100%",
     maxWidth: 650,
     margin: "0 auto",
-    transform: isPreviewMode ? "translateX(80px)" : "translateX(-24px)",
+    transform: isPreviewMode ? "translateX(80px)" : "translateX(-19px)",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
